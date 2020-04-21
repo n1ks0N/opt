295c23cbe8ded3a88e41465bf4a98722ebe89bb2
niksON <gromyakov05@mail.ru>
2020-04-21 21:34:47 +0300

    master

--- js/main.js
+++ js/main.js
@@ -7,6 +7,7 @@ $(document).ready(function() {
         let headerChangeHeight = mainHeight - 64; // minus header.height
         if ($('body, html').scrollTop() >= headerChangeHeight) {
             $('header').addClass('headerScroll');
+            $('.cards').css('opacity', '1');
         } else {
             $('header').removeClass('headerScroll');
         }
@@ -14,6 +15,16 @@ $(document).ready(function() {
             $('.up').css('display', 'block');
         } else {
             $('.up').css('display', 'none');
+        }
+
+        let cards = headerChangeHeight - 400;
+        if ($('body, html').scrollTop() >= cards) {
+            $('.cards').addClass('opacity');
+        }
+
+        let advantages = headerChangeHeight + 400;
+        if ($('body, html').scrollTop() >= advantages) {
+            $('.advantages-content').addClass('opacity');
         }
     });
 });
